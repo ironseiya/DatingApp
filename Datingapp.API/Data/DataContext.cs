7177357970025944e84ab1a51e593ab9ee030025
ironseiya <seiya.liulin@qq.com>
2018-08-26 21:43:03 +0800

    Initial Commit

---
+++ Datingapp.API/Data/DataContext.cs
@@ -0,0 +1,16 @@
+using Datingapp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Datingapp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Value> Values { get; set; }
+        
+    }
+}
